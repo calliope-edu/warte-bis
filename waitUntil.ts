3d261920b514cbbc960ee2cd9d5904160e56f0f9
Jørn Alraun <ja@urbn-pockets.com>
2024-02-01 15:29:00 +0100

    Update waitUntil.ts

--- waitUntil.ts
+++ waitUntil.ts
@@ -1,8 +1,8 @@
 /**
 * Wait Until Custom Blocks
 */
-//% weight=100 color=#bc8cbe block="WaitUntilBlocks" icon="\uf1a7"
-//% groups=['micro:bit V1 and V2', 'micro:bit V2']
+//% weight=100 color=#bc8cbe block="warte bis..." icon="\uf1a7"
+//% groups=['Calliope mini']
 namespace WaitUntilBlocks {
     /**
        * Wait until a pin is pressed
@@ -56,7 +56,7 @@ namespace WaitUntilBlocks {
     //% level.loc.nl="Geluidsniveau waar je op wacht, bijvoorbeeld 80"
     //% level.min=0 level.max=255 level.defl=80
     //% weight=75
-    //% group="micro:bit V2"
+    //% group="Calliope mini"
     export function waitUntilSoundLevelBelow(level: number): void {
         while (input.soundLevel() >= level) {
             basic.pause(20);
@@ -77,7 +77,7 @@ namespace WaitUntilBlocks {
     //% level.loc.nl="Geluidsniveau waar je op wacht, bijvoorbeeld 80"
     //% level.min=0 level.max=255 level.defl=80
     //% weight=80
-    //% group="micro:bit V2"
+    //% group="Calliope mini"
     export function waitUntilSoundLevelAbove(level: number): void {
         while (input.soundLevel() <= level) {
             basic.pause(20);
